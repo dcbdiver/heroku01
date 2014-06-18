881224542438e8ff98e67205ad901ff25c5f977f
unknown <davidcb@DSI-DCONTRERAS.upcont.es>
2014-06-18 12:12:55 +0200

    Con cambios

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -17,7 +17,7 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
         
-        out.write("Hello Heroku".getBytes());
+        out.write("Hello Heroku DCB".getBytes());
         out.flush();
         out.close();
     }
