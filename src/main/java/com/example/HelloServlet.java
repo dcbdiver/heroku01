ab291bd3e553e1518edf366394e17ea4886a8953
dcbdiver <dcb.diver@gmail.com>
2014-06-18 12:41:17 +0200

    Con cambios 2

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -17,7 +17,7 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
         
-        out.write("Hello Heroku DCB".getBytes());
+        out.write("Hello Heroku DCB cambio 2".getBytes());
         out.flush();
         out.close();
     }
